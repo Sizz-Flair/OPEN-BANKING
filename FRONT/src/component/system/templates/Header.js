abea65e595f8d806cad554aad5db3ba20cb95604
Sizz-Flair <akfur222@naver.com>
2021-02-25 00:01:31 +0900

    type: style: Head side design change & mobile size change body: Change to fit the mobile aspect ratio

--- FRONT/src/component/system/templates/Header.js
+++ FRONT/src/component/system/templates/Header.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import '../common.css';
+import {AiOutlineLogout} from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 
-const Header = () => {
+const Header = ({logout}) => {
+
     return (
         <div className="col-sm-12 col-md-12 col-lg-12 col-xl-12 header p-0 mb-3">
             <div className="header-top">
-                <b>test</b>
+                <div className="header-inline">
+                    <ul className="header-gourp">
+                        <li className="header-menu"><Link className="navMenu" to="/login">사용자 정보 변경</Link></li>
+                        <li className="header-menu"><Link className="navMenu" to="/login">공지사항</Link></li>
+                        <li className="header-menu" onClick={logout}><Link className="navMenu" to="/login">로그아웃</Link></li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
 }
 
 export default Header;
-
--- FRONT/src/component/system/templates/Nav.js
+++ FRONT/src/component/system/templates/Nav.js
@@ -1,36 +1,77 @@
-import React from 'react';
+import React,{useState, useEffect, useRef} from 'react';
 import { Link } from 'react-router-dom';
+import styled, {keyframes} from 'styled-components'
 import '../common.css';
 
-const Navbar = () => {
-
+const Navbar = () => { 
+    const [menuState, setMenuState] = useState(false);
+    
+    useEffect(() => {
+      }, []);
+   
     return(      
-        <div className="col-12">
-            <ul className="nav flex-column">
-                <li className="nav-item">
-                    <Link className="navMenu" to="/index/userinfo">사용자 정보</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="navMenu" to="/index/account">잔액조회</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="navMenu" to="/index/transcation">거래내역 조회</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="navMenu" to="/index/accountinfo">계좌정보 변경</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="navMenu" to="/index/user">참가은행 조회</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="navMenu" to="/index/transfer">입금이체</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="navMenu"to="/index/user">이체결과 조회</Link>
-                </li>
-            </ul>
+        <div className={`col-xs-2 col-sm-2 col-md-2 col-lg-2 col-xl-2 border-right navcontainer`}
+        >
+            <div className="col-12"> 
+                <MenuDrop test={menuState} >
+                {console.log(menuState,"width")}
+                <ul className="nav flex-column text-end">
+                    <li className="nav-item">
+                        <Link className="navMenu" to="/index/userinfo">사용자 정보</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="navMenu" to="/index/account">잔액조회</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="navMenu" to="/index/transcation">거래내역 조회</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="navMenu" to="/index/accountinfo">계좌정보 변경</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="navMenu" to="/index/transfer">입금이체</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="navMenu"to="/index/result">이체결과 조회</Link>
+                    </li>
+                </ul>
+                </MenuDrop>
+            </div>
         </div>
     )
 }
+
+const down = keyframes`
+    0% {
+    height: 0px;
+    }
+    100% {
+    height: 200px;
+    }`;
+
+const MenuDrop = styled.div`
+@media ( max-width : 420px) {
+    text-align: center;
+    overflow: hidden;
+    padding-top: 20px;
+    height: 0px;
+    margin-bottom: 20px;
+    &:hover {
+    animation-name: ${down};
+    animation-duration:0.7s;
+    animation-fill-mode: both;
+    }
+    &:before {
+        content: "menu";
+        position: relative;
+        text-align: center;
+        width: 10px;
+        height: 10px;
+        display: initial;
+        border-top: 0.5px solid;
+        border-bottom: 0.5px solid;
+        top: -25px;
+    }
+}`;
 
 export default Navbar;
